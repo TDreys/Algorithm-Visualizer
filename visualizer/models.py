ea7d9122acff6f51de22491ddbf3029cbfe50154
Tom <txd844@student.bham.ac.uk>
2022-10-28 00:29:55 +0100

    changes

--- visualizer/models.py
+++ visualizer/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 class Animation(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     animations = models.TextField(blank=True)
